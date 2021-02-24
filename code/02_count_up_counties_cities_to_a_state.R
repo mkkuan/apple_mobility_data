d08edef3a7dd4695bce0806ef13be2bbb37bc3e3
Mikaela Kuan <mkkuan@dons.usfca.edu>
2021-02-24 10:33:32 -0800

    Add code for counting cities and counties by transportation

---
+++ code/02_count_up_counties_cities_to_a_state.R
@@ -0,0 +1,19 @@
+# the goal of this script is to use the dplyr package to count up the
+# number of cities and counties in a given state that have detailed
+# Apple mobility data available and then eventually to save it to a csv
+
+# Mikaela Kuan
+# February 24, 2021
+# mkkuan@dons.usfca.edu
+
+# load function
+source(file = "code/functions/count_up_counties_cities_transportation.R")
+
+# test the function
+for (state_to_subset in c("California", "Nevada", "Washington", "Oregon",
+                          "Arizona")) {
+  subset_data_count_city_county(input_file_name = paste0(
+                                  "data/raw_data/",
+                                  "applemobilitytrends-2021-02-22.csv"),
+                                state_to_subset)
+}
---
+++ code/archived_versions/02_count_up_counties_cities_in_CA.R
@@ -0,0 +1,29 @@
+# the goal of this script is to use the dplyr package to count up the
+# number of cities and counties in a given state that have detailed
+# Apple mobility data available and then eventually to save it to a csv
+
+# Mikaela Kuan
+# February 24, 2021
+# mkkuan@dons.usfca.edu
+
+# load the package "dplyr"
+library("dplyr")
+
+# we would like to get a count of the number of cities and the
+# number of counties in a given state that have driving mobility
+# data. The input to this will be the output of the previous script
+# namely the state csv files that were already subsetted
+
+# load in the dataset from the previous script
+input_file_name <- "output/applemobilitytrends-2021-02-17_California.csv"
+state_data <- read.csv(input_file_name)
+
+# starting off with dplyr chains
+count_cities_counties_by_type <- state_data %>%
+  select(geo_type, region, transportation_type) %>%
+  group_by(geo_type, transportation_type) %>%
+  tally()
+
+# write out the result of the dplyr chain
+write.csv(count_cities_counties_by_type,
+          "output/california_cities_counties_counts.csv")
