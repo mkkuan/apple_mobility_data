2724597e43c55f78265c7f86eb790dee2a41a9d7
Mikaela Kuan <mkkuan@dons.usfca.edu>
2021-02-24 10:29:26 -0800

    Update code with for loop

--- code/01_subset_data_to_a_state.R
+++ code/01_subset_data_to_a_state.R
@@ -11,6 +11,9 @@
 source("code/functions/subset_mobility_data_to_state.R")
 
 #test out the use of the function
-subset_mobility_data_to_state(
-  input_file_name = "data/raw_data/applemobilitytrends-2021-02-17.csv",
-  state_to_subset = "California")
+for (state_to_subset in c("California", "Nevada", "Washington", "Oregon",
+                          "Arizona")) {
+  subset_mobility_data_to_state(
+    input_file_name = "data/raw_data/applemobilitytrends-2021-02-22.csv",
+    state_to_subset)
+}
