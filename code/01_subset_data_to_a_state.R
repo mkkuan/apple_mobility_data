7e10d005b63b6698dc1909851907d78c9d3092ac
Mikaela Kuan <mkkuan@dons.usfca.edu>
2021-03-03 22:22:04 -0800

    Update R script for subsetting data

--- code/01_subset_data_to_a_state.R
+++ code/01_subset_data_to_a_state.R
@@ -9,11 +9,16 @@
 
 # load functions
 source("code/functions/subset_mobility_data_to_state_wide.R")
+source("code/functions/subset_mobility_data_to_state_long.R")
 
 #test out the use of the function
 for (state_to_subset in c("California", "Nevada", "Washington", "Oregon",
                           "Arizona")) {
   subset_mobility_data_to_state(
+    input_file_name = "data/raw_data/applemobilitytrends-2021-03-02.csv",
+    state_to_subset)
+  
+  subset_data_wide_to_long(
     input_file_name = "data/raw_data/applemobilitytrends-2021-03-02.csv",
     state_to_subset)
 }
