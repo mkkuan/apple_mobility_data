8c3c427e69eb1d18114f4bff950fa66394e998b3
Mikaela Kuan <mkkuan@dons.usfca.edu>
2021-03-03 20:53:16 -0800

    Add long and wide subset functions

---
+++ code/functions/subset_mobility_data_to_state_long.R
@@ -0,0 +1,41 @@
+# the goal of this function is to make the subset data of a state of the
+# Apple mobility data from wide format to long format.
+
+# Mikaela Kuan
+# March 3, 2021
+# mkkuan@dons.usfca.edu
+
+# create a function
+subset_data_wide_to_long <- function(input_file_name, state_to_subset) {
+
+  # read in the wide format subsetted data using readr
+  wide_state_data <- readr::read_csv(file = paste0("output/",
+                                                   "subsetted_states_wide/",
+                                                   tools::file_path_sans_ext(
+                                                     basename(input_file_name)),
+                                                   "_",
+                                                   state_to_subset,
+                                                   "_wide.csv"))
+
+  # use tidyr to edit the column name using "XYYYY.MM.DD" wide format
+  # to long format. The dates will have its own column.
+  long_state_data <- tidyr::pivot_longer(wide_state_data,
+                                         cols = starts_with("202"),
+                                         names_to = "date",
+                                         values_to = "relative_mobility")
+
+  # add a defensive mechanism to stop data that has no information in it from
+  # being written
+  if (nrow(long_state_data) == 0) {
+    stop("ERROR: No rows were detected. Did you input the wrong file?")
+  }
+
+  # write the long format data to a csv file
+  readr::write_csv(long_state_data, file = paste0("output/",
+                                                  "subsetted_states_long/",
+                                                  tools::file_path_sans_ext(
+                                                    basename(input_file_name)),
+                                                  "_",
+                                                  state_to_subset,
+                                                  "_long.csv"))
+}
---
+++ code/functions/subset_mobility_data_to_state_wide.R
@@ -0,0 +1,38 @@
+# Script to read in Apple mobility data from a csv file
+# and subset the data to just include rows from any particular US state
+# and then write out a csv file that only has that subset
+
+# Mikaela Kuan
+# mkkuan@dons.usfca.edu
+# March 3, 2021
+
+# create a function to subset any US state out of the full dataset
+# this should also create an output CSV file that is named based on the state
+# that is subsetted
+subset_mobility_data_to_state <- function(input_file_name,
+                                          state_to_subset) {
+  # read in the complete csv file
+  all_covid_data <- readr::read_csv(input_file_name)
+
+  #change the name sub-region to sub_region
+  all_covid_data <- dplyr::rename(all_covid_data, sub_region = "sub-region")
+
+  # subset the dataset to only include the sub_region column has
+  # the state name in it but we want all columns.
+  state_data <- dplyr::filter(all_covid_data,
+                              .data$sub_region == state_to_subset)
+
+
+  # check that the subsetted data actually has data in it
+  if (nrow(state_data) == 0) {
+    stop("ERROR: No rows matching given state name. Did you make a typo?")
+  }
+
+  # save the state data to a new csv file in the output directory
+  readr::write_csv(state_data, file = paste0("output/subsetted_states_wide/",
+                                      tools::file_path_sans_ext(
+                                        basename(input_file_name)),
+                                      "_",
+                                      state_to_subset,
+                                      "_wide.csv"))
+}
