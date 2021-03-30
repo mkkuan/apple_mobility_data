3dd300a45459692bc3fd7172dadef8857f98b110
Mikaela <mkkuan@usfca.edu>
2021-03-29 19:14:02 -0700

    Add commandline parameters into bash script

--- run_analyze_covid_pipeline.sh
+++ run_analyze_covid_pipeline.sh
@@ -2,7 +2,14 @@
 
 # A bash script to drive the rendering of an Rmarkdown file using some
 # parameters passed in from the shell
+# this script expects two command line parameters
+# the first should be a US state, and the ssecond is the raw apple mobility
+# data csv file downloaded from apple.com
+
+# Mikaela Kuan
+# March 29, 2021
+# mkkuan@dons.usfca.edu
 
 Rscript -e "rmarkdown::render('Analysis.Rmd', \
-            params = list(state = 'Arizona',\
-            data = 'data/raw_data/applemobilitytrends-2021-02-22.csv'))"
+            params = list(state = '$1',\
+            data = '$2'))"
