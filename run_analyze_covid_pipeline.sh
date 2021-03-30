f8b727151d7498d6290a5cdb7d13fdb8d0f67e76
Mikaela Kuan <mkkuan@dons.usfca.edu>
2021-03-29 19:47:09 -0700

    Modify bash script to add spacing

--- run_analyze_covid_pipeline.sh
+++ run_analyze_covid_pipeline.sh
@@ -2,6 +2,7 @@
 
 # A bash script to drive the rendering of an Rmarkdown file using some
 # parameters passed in from the shell
+
 # this script expects two command line parameters
 # the first should be a US state, and the ssecond is the raw apple mobility
 # data csv file downloaded from apple.com
