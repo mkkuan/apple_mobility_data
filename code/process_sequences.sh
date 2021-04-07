65d6b1a36cdf2556114eb4e63054a85729b51850
Mikaela Kuan <mkkuan@dons.usfca.edu>
2021-04-07 08:49:46 +0000

    Modify to include total with >/<29000 bases

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -28,3 +28,15 @@ done
 # this is to tally up the number of SARS-CoV-2 sequences in the dataset from each country and sort this output 
 # from largest to smallest
 zcat $1 | grep ">" | cut -f 5 -d "|" | sort | uniq -c | sort -rn 
+
+# this is to count up the number of sequences in the file that have more than 29,000 base pairs in them
+bioawk -c fastx '{print length($seq)}' $1 | awk '$seq>29000' | wc -l | while read greater
+do
+	echo "There are a total of $greater sequences with more than 29000 bases."
+done
+
+# this is to count up the number that have less than 29,000 base pairs
+bioawk -c fastx '{print length($seq)}' $1 | awk '$seq<29000' | wc -l | while read lesser
+do
+	echo "There are a total of $lesser sequences with less than 29000 bases."
+done
