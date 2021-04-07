b78f7495a8a825339e9ce63e1b77d3285a3b54f4
Mikaela Kuan <mkkuan@dons.usfca.edu>
2021-04-07 09:17:08 +0000

    Edit for proper syntax

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -20,23 +20,23 @@ fi
 file_name=$1
 
 # this is to calculate and report out the total number of sequences in the file
-zcat $1 | bioawk -c fastx '{print $name}' | wc -l | while read line
+zcat "$file_name" | bioawk -c fastx '{print $name}' | wc -l | while read -r line
 do
 	echo "There are a total of $line sequences."
 done
 
 # this is to tally up the number of SARS-CoV-2 sequences in the dataset from each country and sort this output 
 # from largest to smallest
-zcat $1 | grep ">" | cut -f 5 -d "|" | sort | uniq -c | sort -rn 
+zcat "$file_name" | grep ">" | cut -f 5 -d "|" | sort | uniq -c | sort -rn 
 
 # this is to count up the number of sequences in the file that have more than 29,000 base pairs in them
-bioawk -c fastx '{print length($seq)}' $1 | awk '$seq>29000' | wc -l | while read greater
+bioawk -c fastx '{print length($seq)}' "$file_name" | awk '$seq>29000' | wc -l | while read -r greater
 do
 	echo "There are a total of $greater sequences with more than 29000 bases."
 done
 
 # this is to count up the number that have less than 29,000 base pairs
-bioawk -c fastx '{print length($seq)}' $1 | awk '$seq<29000' | wc -l | while read lesser
+bioawk -c fastx '{print length($seq)}' "$file_name" | awk '$seq<29000' | wc -l | while read -r lesser
 do
 	echo "There are a total of $lesser sequences with less than 29000 bases."
 done
